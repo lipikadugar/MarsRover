573e6f70d712cda34d77e521bc495eb00d3e0583
lipikadugar <lipikad@thoughtworks.com>
2015-07-31 11:09:47 +0530

    |#0007| moves forward when rover is facing south.

--- src/com/rover/Forward.java
+++ src/com/rover/Forward.java
@@ -2,22 +2,29 @@ package com.rover;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Forward {
 
-    private final int positionX;
-    private final int positionY;
+    private int positionX;
+    private int positionY;
+    private String direction;
 
-    public Forward(int positionX, int positionY) {
+    public Forward(int positionX, int positionY, String direction) {
 
         this.positionX = positionX;
         this.positionY = positionY;
+        this.direction = direction;
     }
 
     public List<Integer> straight() {
         ArrayList<Integer> position = new ArrayList<>();
+        if (Objects.equals(direction, "N"))
+            positionY += 1 ;
+        else if (Objects.equals(direction, "S"))
+            positionY -= 1 ;
         position.add(positionX);
-        position.add(positionY + 1);
+        position.add(positionY);
         return position;
     }
 }
--- test/com/rover/ForwardTest.java
+++ test/com/rover/ForwardTest.java
@@ -10,15 +10,22 @@ public class ForwardTest {
 
     @Test
     public void shouldYCoordinateIncreaseIfRoverIsFacingNorthDirection() {
-        Forward move = new Forward(2, 3);
+        Forward move = new Forward(2, 3, "N");
 
         assertEquals(Arrays.asList(2, 4), move.straight());
     }
 
     @Test
     public void shouldBe3And7IfRoverPositionIs3And6AndFacingNorthDirection() {
-        Forward move = new Forward(3, 6);
+        Forward move = new Forward(3, 6, "N");
 
         assertEquals(Arrays.asList(3, 7), move.straight());
+    }
+
+    @Test
+    public void shouldBe2And5IfRoverPositionIs2And7AndFacingSouthDirection() {
+        Forward move = new Forward(2, 6, "S");
+
+        assertEquals(Arrays.asList(2, 5), move.straight());
     }
 }
